f370648b530641d3914bc63cfdb45c0c3c91797e
mppfiles <mppfiles@gmail.com>
2014-09-24 03:24:11 -0300

    agrego un reset

--- public/js/cursos.js
+++ public/js/cursos.js
@@ -22,6 +22,7 @@ var CursosModule = {
         
         self.$form_nuevo_requisito.bind('ajax:success', function(evt, xhr, status) {
             $(xhr).insertAfter($("div.lista-requisitos .nuevo"));
+            self.$form_nuevo_requisito.get(0).reset();
         });
 
         self.$form_nuevo_requisito.bind('ajax:error', function(event, xhr, settings) {
