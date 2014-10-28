2e738aa30b1f0a1e19a46f8c26d39ad4c9363ebd
mppfiles <mppfiles@gmail.com>
2014-10-28 02:47:33 -0300

    validaciones de form de carrera

--- app/views/inscripciones/carreras/form.blade.php
+++ app/views/inscripciones/carreras/form.blade.php
@@ -12,7 +12,7 @@
 </style>
 <div class="row">
     <div class="col-md-12">
-     {{ Form::model($obj, ['route' => ['ofertas.inscripciones.'.$route_name, $route_params], 'method' => $method, 'autocomplete' => 'off']) }}
+     {{ Form::model($obj, ['route' => ['ofertas.inscripciones.'.$route_name, extract($route_params)], 'method' => $method, 'autocomplete' => 'off']) }}
      {{ Form::hidden('oferta_formativa_id', $oferta->id) }}   
         <table align="center" cellpadding="10" cellspacing="10" class="table-bordered" style="width: 100%;">
             <thead><tr style="text-align: center; background-color: #bdc3c7; color: #FFFFFF">
