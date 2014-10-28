4d23db62a84d8d4639ef8b5a9f7748a6dfa67d80
mppfiles <mppfiles@gmail.com>
2014-10-28 02:42:32 -0300

    validaciones de form de carrera

--- app/models/InscripcionCarrera.php
+++ app/models/InscripcionCarrera.php
@@ -81,13 +81,13 @@ class InscripcionCarrera extends Eloquent {
         
     );
     
-    public static $enum_tipo_residencia      = array('1' => 'Casa', '2' => 'Depto.', '3' => 'Pensión', '4' => 'Residencia');
-    public static $enum_tipo_establecimiento = array('1' => 'Estatal', '2' => 'Privado');
-    public static $enum_situacion_laboral    = array('1' => 'Trabaja', '2' => 'No trabaja', '3' => 'Desocupado');
-    public static $enum_situacion_laboral_ocupacion  = array('1' => 'Trabajo temporal', '2' => 'Trabajo permanente');
-    public static $enum_situacion_laboral_horas_semana = array('1' => 'Menos de 20', '2' => 'Entre 21 y 35', '36 o más');
-    public static $enum_situacion_laboral_relacion_trabajo_carrera = array('1' => 'Total', '2' => 'Parcial', '3' => 'Ninguna');
-    public static $enum_vive = array('1' => 'SI', '2' => 'NO', '3' => 'NS/NC');
+    public static $enum_tipo_residencia      = array('CASA' => 'Casa', 'DEPTO' => 'Depto.', 'PENSION' => 'Pensión', 'RESIDENCIA' => 'Residencia');
+    public static $enum_tipo_establecimiento = array('ESTATAL' => 'Estatal', 'PRIVADO' => 'Privado');
+    public static $enum_situacion_laboral    = array('TRABAJA' => 'Trabaja', 'NO TRABAJA' => 'No trabaja', 'DESOCUPADO' => 'Desocupado');
+    public static $enum_situacion_laboral_ocupacion  = array('TEMPORAL' => 'Trabajo temporal', 'PERMANENTE' => 'Trabajo permanente');
+    public static $enum_situacion_laboral_horas_semana = array('MENOS DE 20' => 'Menos de 20', 'ENTRE 21 Y 35' => 'Entre 21 y 35', '36 O MAS' => '36 o más');
+    public static $enum_situacion_laboral_relacion_trabajo_carrera = array('TOTAL' => 'Total', 'PARCIAL' => 'Parcial', 'NINGUNA' => 'Ninguna');
+    public static $enum_vive = array('SI' => 'SI', 'NO' => 'NO', 'NS/NC' => 'NS/NC');
     
     public function oferta()
     {
