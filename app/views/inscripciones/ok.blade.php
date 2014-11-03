f5d87a49d93f6b0279b17aa27dae1aa646cb4d0c
mppfiles <mppfiles@gmail.com>
2014-11-03 15:40:49 -0300

    un retoque al template de OK

--- app/views/inscripciones/ok.blade.php
+++ app/views/inscripciones/ok.blade.php
@@ -3,15 +3,14 @@
 @section('main')
 
 <div class="container">
-<div class="row">
-<div class="col-xs-12 col-md-12">
-<div class="col-xs-6 col-md-4">
-
-<blockquote><span class="lead">Inscripción completa</span><span class="glyphicon glyphicon-ok"></span>
-¡Gracias! <br><hr>
-
-<button type="button" class="btn btn-default"><a href="http://udc.edu.ar">Portal UDC</a></button><span class="glyphicon glyphicon-chevron-right"></span></blockquote>
-</div>
-</div>
+    <div class="row">
+        <div class="col-xs-8 col-md-8">
+            <blockquote>
+                <span class="lead">Inscripción completa</span><span class="glyphicon glyphicon-ok"></span>
+                ¡Gracias! <br><hr>
+                <p>En breve recibirá un correo con los pasos para ingresar.</p>
+                <button type="button" class="btn btn-default"><a href="http://udc.edu.ar">Portal UDC</a></button><span class="glyphicon glyphicon-chevron-right"></span></blockquote>
+        </div>
+    </div>
 </div>
 @stop
