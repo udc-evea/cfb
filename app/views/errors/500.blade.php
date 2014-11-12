1bf0f30561f7fae356d03004d04fd535d723bd01
mppfiles <mppfiles@gmail.com>
2014-11-11 22:56:04 -0300

    agrego pantallas de error

--- app/views/errors/500.blade.php
+++ app/views/errors/500.blade.php
@@ -1,1 +1,49 @@
-Error del servidor.
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>UDC :: CFB</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="shortcut icon" href="/favicon.png?v=2">
+        <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}" media="all">
+        <link rel="stylesheet" href="{{asset('font-awesome/css/font-awesome.min.css')}}" media="all">
+        <link rel="stylesheet" href="{{asset('css/main.css')}}" media="all">
+  </head>
+  <body>
+        <div id="container" class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+
+                    <br /><br />
+                    <img src="{{asset('img/LOGO-200x60px.png')}}" />
+
+                    <br /><br />
+                    <h2>&iexcl;Disculp&aacute;! Hubo un error en el servidor</h2>
+                    <button onclick="goBack()">Intent&aacute; de nuevo por favor</button>
+                    <script>function goBack(){window.history.back()}</script>
+
+                    <div class="row">
+                      <div class="col-md-4"></div>
+                      <div class="col-md-4">
+                          <p>Nos ayudar&iacute;a mucho que nos cuentes cu&aacute;l fue el problema
+                          que tuviste. Envianos un mail con los detalles a <a href="mailto:feedback@udc.edu.ar">feedback@udc.edu.ar</a>.</p><br />
+                          <p>En caso de que el error persista, record&aacute; que pod&eacute;s inscribirte
+                              personalmente en <strong>Lewis Jones 248</strong> (RW), de lunes a viernes entre las 8
+                    y las 14 horas, o en <strong>Luis Costa 290</strong> (RW) entre las 13 y las 19 horas.</p>
+                            <br /><img src="{{asset('img/problem786.png')}}" /><p></p>
+                            <p>Para consultas, por favor envi&aacute; un e-mail a <a href="mailto:universidad@udc.edu.ar">universidad@udc.edu.ar</a>.</p><br /></div>
+                      <div class="col-md-4"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+      <div class="container">
+      <div class="col-md-2"><a href="http://udc.edu.ar" target="_blank"><img src="{{asset('img/UDC-120-37-gray.png')}}"></a></div>
+      <div class="col-md-2"><a href="http://www.chubut.gov.ar" target="_blank"><img src="{{asset('img/chubut-oficial-125-24-gray.png')}}"></a></div>
+      <div class="col-md-8"> <p class="small">Creado por Universidad del Chubut. &COPY; 2014 UDC :: Derechos Reservados.<br>
+Lewis Jones 248 (9103) - Rawson, Chubut, Patagonia Argentina.</p></div>
+      </div>
+    </div>
+    </body>
+</html>
