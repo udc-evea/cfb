9e49243966efa25d36cfa296dbe629d997a24eb0
mppfiles <mppfiles@gmail.com>
2014-11-12 02:04:18 -0300

    amplío los documentos a 10 dígitos

--- app/models/InscripcionCarrera.php
+++ app/models/InscripcionCarrera.php
@@ -14,7 +14,7 @@ class InscripcionCarrera extends Eloquent {
     public static $rules = array(
         'oferta_formativa_id'   => 'required|exists:oferta_formativa,id',
         'tipo_documento_cod' => 'required|exists:repo_tipo_documento,id',
-        'documento' => 'required|integer|between:1000000,99999999|unique_with:inscripcion_carrera,oferta_formativa_id,tipo_documento_cod,documento',
+        'documento' => 'required|integer|between:1000000,9999999999|unique_with:inscripcion_carrera,oferta_formativa_id,tipo_documento_cod,documento',
         'apellido' => 'required|between:2,100|regex:/^[\s\'\pLñÑáéíóúÁÉÍÓÚüÜçÇ]+$/',
         'nombre' => 'required|between:2,100|regex:/^[\s\'\pLñÑáéíóúÁÉÍÓÚüÜçÇ]+$/',
         'sexo'   => 'required|in:M,F',
