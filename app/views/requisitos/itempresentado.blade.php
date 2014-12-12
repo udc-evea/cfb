a2137872a0149bafe485574d9eec197b872eef47
mppfiles <mppfiles@gmail.com>
2014-12-11 21:33:58 -0300

    el ABM de requisitos quedó más lindo ahora.

--- app/views/requisitos/itempresentado.blade.php
+++ app/views/requisitos/itempresentado.blade.php
@@ -1,25 +1,29 @@
 <?php $cargado = false; ?>
 @foreach($presentados as $pres)
-    @if($pres->requisito_id == $requisito->id)
-        <?php $cargado = true;?>
-        <?php $pres1 = $pres;?>
-    @else
-        <?php continue;?>
-    @endif
+@if($pres->requisito_id == $requisito->id)
+<?php $cargado = true; ?>
+<?php $pres1 = $pres; ?>
+@else
+<?php continue; ?>
+@endif
 @endforeach
 
 @if($cargado)
-{{ $pres1->fecha_presentacion }}
-<a href="{{ route('ofertas.inscripciones.requisito_borrar', array($oferta->id, $inscripcion->id, $pres1->id)) }}" class="btn btn-default btn-sm action-borrar" data-method="delete" data-remote="true"><span class="glyphicon glyphicon-remove"></span></a>
+<div class="input-group">
+    <span class="form-control-static input-xs">{{ $pres1->fecha_presentacion }}</span>
+    <span class="input-group-btn">
+        <a href="{{ route('ofertas.inscripciones.requisito_borrar', array($oferta->id, $inscripcion->id, $pres1->id)) }}" class="btn btn-warning btn-xs action-borrar" data-method="delete" data-remote="true"> Anular</a>
+    </span>
+</div>
 @else
 <form action="{{ route('ofertas.inscripciones.requisito_presentar', array($oferta->id, $inscripcion->id)) }}" class="nuevo" data-remote="true" method="post">
-<input type="hidden" name="requisito_id" value="{{ $requisito->id}}"/>
+    <input type="hidden" name="requisito_id" value="{{ $requisito->id}}"/>
 
-<div class="input-group">
-<input type="text" id="fecha_presentacion_{{$requisito->id}}" name="fecha_presentacion" class="input-sm form-control fecha"/>
-<span class="input-group-btn">
-<button class="btn btn-default input-sm" type="submit"><span class="glyphicon glyphicon-ok"></span></button>
-</span>
-</div>
+    <div class="input-group">
+        <input type="text" id="fecha_presentacion_{{$requisito->id}}" name="fecha_presentacion" class="form-control input-xs fecha" value="{{ date("d/m/Y") }}"/>
+        <span class="input-group-btn">
+            <button class="btn btn-primary btn-xs" type="submit">Presentar</button>
+        </span>
+    </div>
 </form>
 @endif
