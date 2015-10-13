efe8a9e70a7826193acdf547744ba969ef029166
Nicolás Fernández <nicof05@gmail.com>
2015-10-13 11:25:30 -0300

    Agrego mi PC (notebook - nico) en el archivo p/hacer debug "bootstrap/start.php"

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('nico-PC', 'devCMS110244'),
+	'local' => array('FliaFernCalf-PC', 'devCMS110244'),
 
 ));
 
