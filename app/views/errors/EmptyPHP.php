ad93b8bec41d5f5de633373ad1c8d57e12a0ce55
root <root@CMS110244.(none)>
2016-10-19 17:08:38 -0300

    desde udc

---
+++ app/views/errors/EmptyPHP.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>UDC :: CFB</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="shortcut icon" href="/favicon.png?v=2">
+        <link rel="stylesheet" href="http://udc.edu.ar/cfb/bootstrap/css/bootstrap.min.css" media="all">
+        <link rel="stylesheet" href="http://udc.edu.ar/cfb/font-awesome/css/font-awesome.min.css" media="all">
+        <link rel="stylesheet" href="http://udc.edu.ar/cfb/jquery-ui-1.11.1.custom/jquery-ui.min.css">
+        <link rel="stylesheet" href="http://udc.edu.ar/cfb/css/main.css" media="all">
+
+        <script src="http://udc.edu.ar/cfb/js/modernizr-2.6.2.min.js"></script>
+        <script src="http://udc.edu.ar/cfb/js/jquery-2.1.1.min.js"></script>
+        <script src="http://udc.edu.ar/cfb/bootstrap/js/bootstrap.min.js"></script>
+        <script src="http://udc.edu.ar/cfb/bootstrap-extras/bootbox.min.js"></script>
+        <script src="http://udc.edu.ar/cfb/jquery-ui-1.11.1.custom/jquery-ui.min.js"></script>
+
+        <script src="http://udc.edu.ar/cfb/js/rails.js"></script>
+        <script src="http://udc.edu.ar/cfb/js/main.js"></script>
+  </head>
+  <body>
+        <div id="container" class="container">
+            <div class="row">
+                <div class="col-md-12">  </div>
+            </div>
+        </div>
+        <div class="footer">
+      <div class="container">
+      <div class="col-md-2"><a href="http://udc.edu.ar" target="_blank"><img src="http://udc.edu.ar/cfb/img/UDC-120-37-gray.png"></a></div>
+      <div class="col-md-2"><a href="http://www.chubut.gov.ar" target="_blank"><img src="http://udc.edu.ar/cfb/img/chubut-oficial-125-24-gray.png"></a></div>
+      <div class="col-md-8"> <p class="small">Creado por Universidad del Chubut. © 2014 UDC :: Derechos Reservados.<br>
+Lewis Jones 248 (9103) - Rawson, Chubut, Patagonia Argentina.</p></div>
+      </div>
+    </div>
+    </body>
+</html>
