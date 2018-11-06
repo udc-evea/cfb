0dff8d91dd01ff63bf4b3304af7310b972d65f5e
Nicolás H. Fernández <nicof05@gmail.com>
2018-11-06 15:45:13 -0300

    Cambios: 1) Agrego mi notebook en la lista de compus 'locales' para el servidor. Nombre PC: 'NicoFernandez' en archivo boostrap/start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 	//'local' => array('FliaFernCalf-PC', 'generica', 'CMS110244'),
-        'local' => array('FliaFernCalf-PC', 'generica', 'Win7'),
+        'local' => array('FliaFernCalf-PC', 'generica', 'Win7','NicoFernandez'),
 ));
 
 /*
